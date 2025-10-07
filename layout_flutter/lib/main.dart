6e4460cc9f674659b60662fd6a13bfe75fa1e62c
Danendra AP <112272169+Danennndraaa@users.noreply.github.com>
2025-10-07 07:57:45 +0700

    Refractor: Change column to listview

--- layout_flutter/lib/main.dart
+++ layout_flutter/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(
+        body: ListView(
           children: [
             imageSection,
             titleSection,
