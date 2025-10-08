af6d169c4bacb6c639df5abf60f52b9d81e38ce5
Danendra AP <112272169+Danennndraaa@users.noreply.github.com>
2025-10-08 23:49:49 +0700

    Tugas Praktikum 2 Soal No 1

--- layout_flutter/lib/widget/item_card.dart
+++ layout_flutter/lib/widget/item_card.dart
@@ -10,7 +10,13 @@ class ItemCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: onTap,
+      onTap: () {
+        Navigator.pushNamed(
+          context,
+          '/item',
+          arguments: item,
+        );
+      },
       child: Card(
         margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         elevation: 4,
